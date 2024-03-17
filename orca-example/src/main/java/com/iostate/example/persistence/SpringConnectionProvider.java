48d4acc88aec410d4aff13ebc85749d1bf06b0d0
Dongqing Hu <dongqing.hu@outlook.com>
2024-03-17 19:02:03 +0800

    integrate Spring transaction manager in the example application (so easy)

--- orca-example/src/main/java/com/iostate/example/persistence/SpringConnectionProvider.java
+++ orca-example/src/main/java/com/iostate/example/persistence/SpringConnectionProvider.java
@@ -2,19 +2,20 @@ package com.iostate.example.persistence;
 
 import com.iostate.orca.api.ConnectionProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.SQLException;
 
 @Component
-public class ConnectionManager implements ConnectionProvider {
+public class SpringConnectionProvider implements ConnectionProvider {
     @Autowired
     private DataSource dataSource;
 
     @Override
-    public Connection getConnection() throws SQLException {
-        return dataSource.getConnection();
+    public Connection getConnection() {
+        // This line gets the thread-bound connection in current transaction
+        return DataSourceUtils.getConnection(dataSource);
     }
 }
